c71fde5605997ab75061d9c380863f0c6a9d076b
Arzo-zi <zinahashraf88@gmail.com>
2025-07-09 00:27:55 +0300

    feat: update OnboardingScreen to include navigation callback and improve UI elements

--- app/src/main/java/com/example/caffeine/presentation/screen/OnboardingScreen.kt
+++ app/src/main/java/com/example/caffeine/presentation/screen/OnboardingScreen.kt
@@ -9,14 +9,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Icon
@@ -37,7 +34,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.caffeine.R
@@ -47,11 +43,14 @@ import com.example.caffeine.ui.theme.DarkSurface
 import kotlinx.coroutines.delay
 
 @Composable
-fun OnboardingScreen(modifier: Modifier = Modifier) {
+fun OnboardingScreen(
+    modifier: Modifier = Modifier,
+    onNavigationToChooseCoffee: () -> Unit
+) {
     Box(
         modifier = modifier
-            .fillMaxSize()
-            .padding(WindowInsets.statusBars.asPaddingValues()),
+            .fillMaxSize(),
+//            .padding(WindowInsets.statusBars.asPaddingValues()),
         contentAlignment = Alignment.Center
     ) {
         LazyColumn(
@@ -76,9 +75,9 @@ fun OnboardingScreen(modifier: Modifier = Modifier) {
         }
         FloatingActionButton(
             text = "bring my coffee",
-//            TODO
-            icon = painterResource(id = R.drawable.coffee),
-            onClick = {},
+            icon = painterResource(id = R.drawable.ic_coffee),
+            onClick = {
+                onNavigationToChooseCoffee()            },
             modifier = Modifier.padding(top = 654.dp, bottom = 50.dp)
         )
     }
@@ -196,10 +195,4 @@ private fun StarIcon(
         }
 
     )
-}
-
-@Preview
-@Composable
-private fun OnboardingScreenPreview() {
-    OnboardingScreen()
 }
